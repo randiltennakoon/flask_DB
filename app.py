6c17c3d6766dd4db4d822dc4e79eb16b38f4df77
randiltennakoon <randil2016al@gmail.com>
2022-05-11 09:36:45 +0530

    created registration form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
 
@@ -9,6 +10,17 @@ app.config['SECRET_KEY'] = '225dccaa4ab7a419f983d2f24821fc93'
 @app.route('/')
 def home():
     return render_template('home.html', title='Home')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
 
 
